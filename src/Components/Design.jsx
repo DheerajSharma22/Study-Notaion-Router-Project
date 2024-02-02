352e1aca66e8580e525eddb81241754203e7c760
Dheeraj Sharma <dsprofessional12@gmail.com>
2024-02-02 15:00:27 +0530

    first commit

---
+++ src/Components/Design.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Design = (props) => {
+  return (
+    <div className="flex items-center justify-center text-richblack-100 text-3xl h-full min-h-screen">
+      {props.title}
+    </div>
+  );
+};
+
+export default Design;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div className="flex justify-center items-center text-white text-3xl min-h-screen">
+      Welcome to Studynotion!
+    </div>
+  );
+}
+
+export default Dashboard;
